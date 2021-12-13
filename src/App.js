e4dcf3673079b179418f894382b6b0d02f5d3ad0
yekijnuy <keyyun2028@gmail.com>
2021-12-13 00:10:48 -0500

    Adding posts client, server, spawn process

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { Box, Button, Grid, Paper, Switch, Typography } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+import b64ToBlob from 'b64-to-blob';
+import fileSaver from 'file-saver';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [downloading, setDownloading] = useState(false);
+    const { handleSubmit, control } = useForm({
+        defaultValues: {
+            checkJson: false,
+            checkDetailed: false,
+        },
+    });
+
+    const onSubmit = ({ checkJson, checkDetailed }) => {
+        setDownloading(true);
+        fetch(`http://localhost:3001/postdata/${checkJson}/${checkDetailed}`)
+            .then((response) => {
+                return response.text();
+            })
+            .then((zipAsBase64) => {
+                const blob = b64ToBlob(zipAsBase64, 'application/zip');
+                fileSaver.saveAs(blob, 'posts.zip');
+                setDownloading(false);
+            });
+    };
+
+    const styleOverride = {
+        color: '#73797d',
+        backgroundColor: '#6FEBBE',
+    };
+
+    return (
+        <Box sx={{ width: '100%', maxWidth: 1400 }}>
+            <Paper style={styleOverride} elevation={3}>
+                <Typography variant='h4' component='div' gutterBottom>
+                    CafeMedia Coding Challenge - Key Houck
+                </Typography>
+            </Paper>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container direction='column' spacing={3}>
+                    <Grid item xs={3}>
+                        <Controller
+                            name='checkJson'
+                            control={control}
+                            rules={{ required: false }}
+                            render={({ field }) => (
+                                <Fragment>
+                                    <Typography>JSON Format</Typography>
+                                    <Switch {...field} label='JSON Format'></Switch>
+                                </Fragment>
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Controller
+                            name='checkDetailed'
+                            control={control}
+                            rules={{ required: false }}
+                            render={({ field }) => (
+                                <Fragment>
+                                    <Typography>Detail Mode</Typography>
+                                    <Switch {...field} label='Detail Mode'></Switch>
+                                </Fragment>
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button type='submit' variant='contained'>
+                            <CheckIcon />
+                            {downloading ? 'Downloading' : 'Download'}
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Box>
+    );
 }
 
 export default App;
